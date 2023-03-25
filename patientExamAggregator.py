c75d679d4915c41fd78904e67f345f2c9b68d545
r3tro7 <72056013+r3tro7@users.noreply.github.com>
2023-03-25 14:46:13 -0700

    Added Methods to Add Records to the System

--- patientExamAggregator.py
+++ patientExamAggregator.py
@@ -23,4 +23,28 @@ class PatientExamSystem:
         self.patients: Dict[str, Patient] = {}
         self.exams: Dict[str, Patient] = {}
 
+    #-----Starting of add_patient() Method-----#
+    def add_patient(self, id: str, name: str) -> None:
+        """
+        Function to add Patient Record to the System
+        Parameters:
+            id(str): Patient ID
+            name(str): Patient Name
+        """
+        if id not in self.patients:
+            self.patients[id] = Patient(id, name)
+    #-----Ending of add_patient() Method-----#
+
+    #-----Starting of add_exam() Method-----#
+    def add_exam(self, patient_id: str, exam_id: str) -> None:
+        """
+        Function to add Examination Record to the System
+        Parameters:
+            patiend_id(str): Patient ID
+        """
+        if patient_id in self.patients and exam_id not in self.exams:
+            self.patients[patient_id].exams.append(Exam(exam_id))
+            self.exams[exam_id] = self.patients[patient_id]
+    #-----Ending of add_exam() Method-----#
+
 #---------------Ending of PatientExamSystem Class---------------#
