025267afc4195c2d84d0168da21c50897368f862
r3tro7 <72056013+r3tro7@users.noreply.github.com>
2023-03-25 15:06:42 -0700

    Added methods to delete records from the system

--- patientExamAggregator.py
+++ patientExamAggregator.py
@@ -17,7 +17,7 @@ class Exam:
 #---------------Starting of PatientExamSystem Class---------------#
 class PatientExamSystem:
     """
-    Manages the Patient Record System
+    Manages the Patient Record System.
     """
     def __init__(self):
         self.patients: Dict[str, Patient] = {}
@@ -26,7 +26,7 @@ class PatientExamSystem:
     #-----Starting of add_patient() Method-----#
     def add_patient(self, id: str, name: str) -> None:
         """
-        Function to add Patient Record to the System
+        Function to add Patient Record to the System.
         Parameters:
             id(str): Patient ID
             name(str): Patient Name
@@ -38,7 +38,7 @@ class PatientExamSystem:
     #-----Starting of add_exam() Method-----#
     def add_exam(self, patient_id: str, exam_id: str) -> None:
         """
-        Function to add Examination Record to the System
+        Function to add Examination Record to the System.
         Parameters:
             patiend_id(str): Patient ID
         """
@@ -46,5 +46,30 @@ class PatientExamSystem:
             self.patients[patient_id].exams.append(Exam(exam_id))
             self.exams[exam_id] = self.patients[patient_id]
     #-----Ending of add_exam() Method-----#
+
+    #-----Starting of del_exam() Method-----#
+    def del_patient(self, id: str) -> None:
+        """
+        Function to delete patient Record from the system.
+        Parameters:
+            id(str): Patient ID
+        """
+        if id in self.patients:
+            for exam in self.patients[id].exams:
+                self.exams.pop(exam.id, None)
+            self.patients.pop(id, None)
+    #-----Ending of del_exam() Method-----#
+
+    #-----Starting of del_exam() Method-----#
+    def del_exam(self, id: str) -> None:
+        """
+        Function to delete Examination record from the System.
+        Parameters:
+            id(str): Examination ID
+        """
+        if id in self.exams:
+            self.exams[id].exams = [exam for exam in self.exams[id].exams if exam.id != id]
+            self.exams.pop(id, None)
+    #-----Starting of del_exam() Method-----#
 
 #---------------Ending of PatientExamSystem Class---------------#
